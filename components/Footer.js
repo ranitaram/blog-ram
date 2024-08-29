acb126e756bf2aec085fd981b07975b46d7e60af
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-08-29 11:46:26 -0500

    agregado las politicas de privacidad y cookies

--- components/Footer.js
+++ components/Footer.js
@@ -5,32 +5,32 @@ export default function Footer() {
         <div className="footer">
             <div className="container flex flex-sb flex-wrap flex-left">
                 <div className="footer_logo" data-aos="fade-right">
-                    <h2>VBMBLOGS</h2>
-                    <h4>&copy; 2024 ALL Rights Reserved.</h4>
-                    <h3>Coded By <span>@vbmcoder</span></h3>
+                    <h2>TiburónCode</h2>
+                    <h4>&copy; 2024 Todos Los Derechos Reservados.</h4>
+                    {/* <h3>Coded By <span>@vbmcoder</span></h3> */}
                 </div>
-                <div className="q_links" data-aos="fade-up">
+                {/* <div className="q_links" data-aos="fade-up">
                     <h3>Quick Links</h3>
                     <ul>
                         <li><Link href='/'>Advertise with us</Link></li>
                         <li><Link href='/'>About Us</Link></li>
                         <li><Link href='/'>Contact Us</Link></li>
                     </ul>
-                </div>
+                </div> */}
                 <div className="q_links" data-aos="fade-up">
                     <h3>Legal Stuff Links</h3>
                     <ul>
-                        <li><Link href='/'>Privacy Notice</Link></li>
-                        <li><Link href='/'>Cookie Policy</Link></li>
+                        <li><Link href='/politica-privacidad'>Política de Privacidad.</Link></li>
+                        <li><Link href='/politica-cookies'>POLÍTICA DE COOKIES</Link></li>
                         <li><Link href='/'>Terms Of Use</Link></li>
                     </ul>
                 </div>
                 <div className="q_links" data-aos="fade-left">
                     <h3>Social Media</h3>
                     <ul>
-                        <li><Link href='/'>Github</Link></li>
+                        <li><Link href='https://github.com/ranitaram'>Github</Link></li>
                         <li><Link href='/'>Twitter</Link></li>
-                        <li><Link href='/'>Instagram</Link></li>
+                        {/* <li><Link href='/'>Instagram</Link></li> */}
                     </ul>
                 </div>
             </div>
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
      <Head >
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo-shark.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
