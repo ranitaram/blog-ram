1398e2eefcd78327809f8621f826639b7e61c878
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-08-31 23:49:59 -0500

    quitando la etiqueta porque rompe el deploy

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
      <Head >
-    
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)};
-  gtag('js', new Date());
-
-  gtag('config', 'G-XFXNVQNPCG');
-</script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-XFXNVQNPCG"></script>
         <link rel="icon" href="/logo-shark.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
